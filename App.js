af3022930f39c19a2aacfc1c5903cf6873983638
dasanwk-ops <dasanwk@gmail.com>
2026-03-31 19:07:53 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+/* 화면 전체를 꽉 채우기 위한 설정 */
+html,
+body,
+#root {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #f8fafc; /* 밝은 회색 배경 */
+}
